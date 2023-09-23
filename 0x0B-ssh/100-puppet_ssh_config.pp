5b64b15aa322a2ce58b79625ad68645c172ccfb2
prosper <mprosp3r@gmail.com>
2023-09-23 13:31:34 -0700

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # puppet to creatie a config file of ssh
 $str = "Host *
    IdentityFile ~/.ssh/school
-   PasswordAuthentication no \n"
+   PasswordAuthentication no\n"
 file {'/root/.ssh/config':
   ensure  => file,
   mode    => '0600',
